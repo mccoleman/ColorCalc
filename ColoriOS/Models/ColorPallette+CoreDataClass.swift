d4cb4aaa4bcc588d04c190cbf6df7ec95cc0a309
Michaiah Coleman <michaiah.coleman@gmail.com>
2023-07-05 14:14:39 -0400

    fix for some file location issues
    adding basic inits may move to a factory service instead

--- ColoriOS/Models/ColorPallette+CoreDataClass.swift
+++ ColoriOS/Models/ColorPallette+CoreDataClass.swift
@@ -1,8 +1,9 @@
 //
 //  ColorPallette+CoreDataClass.swift
-//  
+//  ColoriOS
 //
 //  Created by Michaiah Coleman on 6/19/23.
+//  Copyright © 2023 Michaiah Coleman. All rights reserved.
 //
 //
 
@@ -12,4 +13,12 @@ import CoreData
 @objc(ColorPallette)
 public class ColorPallette: NSManagedObject {
 
+    static func basicInit(moc:NSManagedObjectContext, title:String, hexString:String = "FFFFFF") -> ColorPallette {
+        let colorPalette = ColorPallette(context: moc)
+        colorPalette.title = title
+        colorPalette.createDate = Date()
+        colorPalette.colorOptions = [ColorOption.basicInit(context: moc, owner: colorPalette,hexString: hexString)]
+        
+        return colorPalette
+    }
 }
